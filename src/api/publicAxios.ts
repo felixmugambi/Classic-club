1b12182ca082a7e80277c62e4a05be336d8d04a5
RESRAS SILEN <mugafelhida98@gmail.com>
2025-07-30 04:36:29 -0700

    Updated PublicAxios

--- src/api/publicAxios.ts
+++ src/api/publicAxios.ts
@@ -3,8 +3,7 @@ import axios from "axios";
 
 const publicAPI = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  withCredentials: true, // <-- important if backend expects cookies
 });
-
-
 
 export default publicAPI;
